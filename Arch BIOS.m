e643ffe8810399f8a3577be853df517a123f061d
Yuri Rublev <rubleviura34@hotmail.com>
2026-09-24 20:54:56 +0300

    Update Arch BIOS.m

--- Arch BIOS.m
+++ Arch BIOS.m
@@ -1375,8 +1375,6 @@ pacman -S --noconfirm vulkan-tools libva-utils mesa-utils mesa-demos glmark2 nvt
 # (Любая комбинация: AMD Radeon + NVIDIA, Intel Iris/UHD + NVIDIA)
 # 📦 1. Установка драйверов NVIDIA и критически важных библиотек EGL для Wayland
 # pacman -S --noconfirm nvidia-open-lts nvidia-utils lib32-nvidia-utils egl-wayland egl-wayland2 nvidia-settings
-# ✅ Полная замена строки HOOKS на безопасную последовательность.
-# sed -i "s|^HOOKS=(.*)|HOOKS=(systemd autodetect microcode modconf keyboard sd-vconsole block filesystems fsck)|" /etc/mkinitcpio.conf
 
 # 📦 2. Установка инструмента управления питанием dGPU
 # switcheroo-control (современный, интеграция в GUI, рекомендуется для Wayland)
